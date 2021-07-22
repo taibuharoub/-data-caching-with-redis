80d42237630002bef1db70cfdb5aa3957f0e918a
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-22 11:10:08 +0300

    Add the model

---
+++ models/Post.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+    },
+    content: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Store", postSchema);
